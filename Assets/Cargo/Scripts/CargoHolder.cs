45e69422c128c42a3ecb49f5e1b4e27cf46fa6d1
Marat Zaripov <49365339+GinoxXP@users.noreply.github.com>
2023-02-13 13:14:37 +0300

    Add visual cargo to car

--- Assets/Cargo/Scripts/CargoHolder.cs
+++ Assets/Cargo/Scripts/CargoHolder.cs
@@ -10,15 +10,32 @@ public class CargoHolder : MonoBehaviour
 
     private List<GameObject> cargoBoxes = new List<GameObject>();
 
+    private void ChangeActiveBoxes()
+    {
+        for (int i = 0; i < cargoBoxes.Count; i++)
+        {
+            if (i < inventory.CurrentCount)
+                cargoBoxes[i].SetActive(true);
+            else
+                cargoBoxes[i].SetActive(false);
+        }
+    }
+
     private void Start()
     {
         for (int i = 0; i < cargoParent.childCount; i++)
         {
             var cargoBox = cargoParent.GetChild(i).gameObject;
             cargoBoxes.Add(cargoBox);
-
-            if (i > inventory.CurrentCount)
-                cargoBox.SetActive(false);
         }
+
+        ChangeActiveBoxes();
+
+        inventory.CurentCountChanged += ChangeActiveBoxes;
+    }
+
+    private void OnDestroy()
+    {
+        inventory.CurentCountChanged -= ChangeActiveBoxes;
     }
 }
